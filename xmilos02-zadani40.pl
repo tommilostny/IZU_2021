3c7444e1c41a65900c17949d53a47b4885bfb008
Tom Milostny <tommilostny@live.com>
2021-03-25 12:41:42 +0100

    Rafactor to remove ';'

--- xmilos02-zadani40.pl
+++ xmilos02-zadani40.pl
@@ -1,11 +1,15 @@
-% 40. Zjištění, zdali se jedná o seznam, ve kterém se pravidelně střídají kladná a záporná čísla.
+% 40. Zjištění, zdali se jedná o seznam,
+%     ve kterém se pravidelně střídají kladná a záporná čísla.
 %
 % Autor: Tomáš Milostný (xmilos02)
 %
 % uloha40(SEZNAM)
 % SEZNAM je seznamem čísel typu Integer různých od nuly
 
-negative([H|T]) :- H < 0 , (positive(T) ; T=[]).
-positive([H|T]) :- H > 0 , (negative(T) ; T=[]).
+negative([]).
+positive([]).
+negative([H|T]) :- H < 0 , positive(T).
+positive([H|T]) :- H > 0 , negative(T).
 
-uloha40(List) :- positive(List) ; negative(List).
+uloha40(List) :- positive(List).
+uloha40(List) :- negative(List).
